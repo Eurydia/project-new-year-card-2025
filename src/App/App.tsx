2c8c678a47dec27135883dfac92f35f9c1a174b4
Eurydia <crossdeltavi@gmail.com>
2025-01-17 10:09:22 +0100

    added missing card from previous backlog

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,74 +1,91 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import {
-  createBrowserRouter,
+  createHashRouter,
   redirect,
   RouterProvider,
 } from "react-router";
+import { encryptDocument } from "../core/crypto";
 import { LoaderData } from "../types/loader";
 import { ContentView } from "../views/ContentView";
 import { HomeView } from "../views/HomeView";
 import { theme } from "./theme";
-
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <HomeView />,
-    },
-    {
-      path: "/card",
-      loader: ({ request }) => {
-        const url = new URL(request.url);
-        const queryParam = url.searchParams;
-        if (!queryParam.has("id")) {
-          return redirect("/");
-        }
-        return redirect(`/card/${queryParam.get("id")!}`);
-      },
-    },
-    {
-      path: "/card/:id",
-      element: <ContentView />,
-      loader: async ({ params }) => {
-        const { id } = params;
-        if (id === undefined) {
-          return redirect("/");
-        }
-        const response = await fetch(
-          `${
-            import.meta.env.BASE_URL
-          }assets/content/${id}.enc`
-        );
-        if (!response.ok) {
-          return redirect("/");
-        }
-        const contentType =
-          response.headers.get("content-type");
-        if (contentType === "text/html") {
-          return redirect("/");
-        }
-        const content = await response.text();
-        const loaderData: LoaderData = {
-          id,
-          content,
-        };
-        return loaderData;
-      },
-    },
-    {
-      path: "*",
-      loader: () => {
-        return redirect("/");
-      },
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: "/project-new-year-card-2025/",
-  }
-);
+    path: "/",
+    element: <HomeView />,
+  },
+  {
+    path: "/card",
+    loader: ({ request }) => {
+      const url = new URL(request.url);
+      const queryParam = url.searchParams;
+      if (!queryParam.has("id")) {
+        return redirect("/");
+      }
+      return redirect(`/card/${queryParam.get("id")!}`);
+    },
+  },
+  {
+    path: "/card/:id",
+    element: <ContentView />,
+    loader: async ({ params }) => {
+      const { id } = params;
+      if (id === undefined) {
+        return redirect("/");
+      }
+      const response = await fetch(
+        `${
+          import.meta.env.BASE_URL
+        }assets/content/${id}.enc`
+      );
+      if (!response.ok) {
+        return redirect("/");
+      }
+      const contentType =
+        response.headers.get("content-type");
+      if (contentType === "text/html") {
+        return redirect("/");
+      }
+      const content = await response.text();
+      const loaderData: LoaderData = {
+        id,
+        content,
+      };
+      return loaderData;
+    },
+  },
+  {
+    path: "*",
+    loader: () => {
+      return redirect("/");
+    },
+  },
+]);
 
 export const App: FC = () => {
+  useEffect(() => {
+    (async () => {
+      const { encryptedDocument, iv } =
+        await encryptDocument(
+          `# _Dear Aiz,_
+
+As 2025 is getting closer, actually, by the time you are reading this message, it is most likely 2025 already.
+
+Nonetheless, I just wanted to take a moment to send you my warmest wishes for 2025. May it bring opportunities, growth, and success. May you be blessed with good health, and fortune. May you be blessed with a reason to laugh and smile in each and every day.
+
+**_Happy new year!_**
+
+Sincerely,
+
+Thanakorn Phuttharaksa
+`,
+          "dear-aiz"
+        );
+      console.debug(encryptedDocument, iv);
+    })();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
